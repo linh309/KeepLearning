a8345b141e062f1c94a693ec4d2bfcd331f12db7
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-05-02 10:05:12 +0700

    use customized view folder by using customized class that inherited from RazorViewEngine

---
+++ WebApplication1/App_Start/CustomViewEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.App_Start
+{
+    public class CustomViewEngine : RazorViewEngine
+    {
+        public CustomViewEngine()
+        {
+            var viewLocations = new string[] 
+            {
+                "~/Views/Pages/{1}/{0}.cshtml"
+            };
+
+            this.ViewLocationFormats = new string[]
+            {
+                "~/Views/Pages/{1}/{0}.cshtml"
+            };
+        }
+    }
+}
--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApplication1.App_Start;
 
 namespace WebApplication1
 {
@@ -12,11 +13,14 @@ namespace WebApplication1
     {
         protected void Application_Start()
         {
-            var razorViewEgine = ViewEngines.Engines.OfType<RazorViewEngine>().FirstOrDefault();
-            razorViewEgine.ViewLocationFormats = new string[]
-            {
-                "~/Views/Pages/{1}/{0}.cshtml"
-            };
+            //var razorViewEgine = ViewEngines.Engines.OfType<RazorViewEngine>().FirstOrDefault();
+            //razorViewEgine.ViewLocationFormats = new string[]
+            //{
+            //    "~/Views/Pages/{1}/{0}.cshtml"
+            //};
+
+            var viewEngines = ViewEngines.Engines;
+            viewEngines.Add(new CustomViewEngine());
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
