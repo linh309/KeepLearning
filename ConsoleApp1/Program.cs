cc4a46fd4ff7a5ed5a0183904ba18ae409024feb
linh.nguyen5 <linh.nguyen5@CL-LINHNGUYEN5>
2018-05-02 09:52:30 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static T GetDefaultValue<T>()
+        {
+            T value = default(T);
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            bool b = GetDefaultValue<bool>();
+            int i = GetDefaultValue<int>();
+            double d = GetDefaultValue<double>();
+
+            Console.WriteLine("{0}, {1}, {2}", b, i, d);
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
